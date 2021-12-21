819f657f32eebedb5e9ddf478d498405a5fd360c
okstring <milkoi7801@gmail.com>
2021-12-21 19:35:57 +0900

    Add: 주석 추가, service 추가

--- Lab/ViewController.swift
+++ Lab/ViewController.swift
@@ -10,12 +10,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    /// A service that can be used to group the tokens
+    /// as the kSecAttrAccount should be unique.
+    let service = "com.bundle.stuff.token-service"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print(Bundle.main.bundleIdentifier)
         print(KeyChain.shared.addItem(id: "ho", pwd: "12345"))
         print(KeyChain.shared.getItem(key: "ho") as? String)
+        print(KeyChain.shared.updateItem(value: "555", key: "ho"))
     }
     
 }
@@ -23,8 +27,11 @@ class ViewController: UIViewController {
 class KeyChain {
     static let shared = KeyChain()
     
+    let service = Bundle.main.bundleIdentifier ?? "com.Lab.secret"
+    
     func addItem(id: Any, pwd: Any) -> Bool {
         let addQuery: [CFString: Any] = [kSecClass: kSecClassGenericPassword,
+                                   kSecAttrService: service,
                                    kSecAttrAccount: id,
                                      kSecValueData: (pwd as AnyObject).data(using: String.Encoding.utf8.rawValue) as Any]
         
@@ -46,6 +53,7 @@ class KeyChain {
     
     func getItem(key: Any) -> Any? {
         let query: [CFString: Any] = [kSecClass: kSecClassGenericPassword,
+                                kSecAttrService: service,
                                 kSecAttrAccount: key,
                            kSecReturnAttributes: true,
                                  kSecReturnData: true]
@@ -66,6 +74,7 @@ class KeyChain {
     
     func updateItem(value: Any, key: Any) -> Bool {
         let prevQuery: [CFString: Any] = [kSecClass: kSecClassGenericPassword,
+                                    kSecAttrService: service,
                                     kSecAttrAccount: key]
         let updateQuery: [CFString: Any] = [kSecValueData: (value as AnyObject).data(using: String.Encoding.utf8.rawValue) as Any]
         
@@ -82,6 +91,7 @@ class KeyChain {
     
     func deleteItem(key: String) -> Bool {
         let deleteQuery: [CFString: Any] = [kSecClass: kSecClassGenericPassword,
+                                      kSecAttrService: service,
                                       kSecAttrAccount: key]
         let status = SecItemDelete(deleteQuery as CFDictionary)
         if status == errSecSuccess { return true }
